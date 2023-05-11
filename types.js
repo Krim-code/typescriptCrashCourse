d3ed0c74037f8063762f7eeafa7531944ff7a5c6
Krim-code <suhetsky.gricha555@gmail.com>
2023-05-11 16:55:33 +0300

    Types is Cool; Started interfaces

---
+++ types.js
@@ -0,0 +1,20 @@
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello TypeScript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ['Hello', 'Typescript'];
+// Tuple
+var contact = ['Bob', 1234567];
+//Any
+var variable = 42;
+variable = 'New String';
+variable = [];
+// ====
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Revi');
